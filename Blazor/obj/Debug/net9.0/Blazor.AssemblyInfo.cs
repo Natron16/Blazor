454f0ddc1d66aa1b8063d6fbeaf4f60b51bfd3d8
Natron16 <144354043+Natron16@users.noreply.github.com>
2025-03-18 08:25:14 -0500

    BMI Page and Equations

--- Blazor/obj/Debug/net9.0/Blazor.AssemblyInfo.cs
+++ Blazor/obj/Debug/net9.0/Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a8ff80f9332e142b3409b504ec0c5df48be8cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
